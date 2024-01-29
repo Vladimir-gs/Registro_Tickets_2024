122b0c8078e52debba525ae53adaec074ca16c1b
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-28 22:30:30 -0400

    Inyectamos nuestro contexto

--- Registro_Tickets_2024/Program.cs
+++ Registro_Tickets_2024/Program.cs
@@ -1,10 +1,16 @@
 using Registro_Tickets_2024.Components;
+using Registro_Tickets_2024.DAL;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+
 
 var app = builder.Build();
 
