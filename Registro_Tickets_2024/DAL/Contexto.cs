cf945d221d45ebc0affc0298ff7f0ef6f7f7355e
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-28 23:57:26 -0400

    Registro de prioridades

--- Registro_Tickets_2024/DAL/Contexto.cs
+++ Registro_Tickets_2024/DAL/Contexto.cs
@@ -5,9 +5,9 @@ namespace Registro_Tickets_2024.DAL
 {
     public class Contexto: DbContext
     {
-        public DbSet<Prioridades> Prioridades { get; set; }
-        public DbSet<Clientes> Clientes { get; set; }
-        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Prioridades>? Prioridades { get; set; }
+        public DbSet<Clientes>? Clientes { get; set; }
+        public DbSet<Tickets>? Tickets { get; set; }
 
         public Contexto(DbContextOptions<Contexto> options): base(options) { }
     }
