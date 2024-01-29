85ff940304d4c10fa17e8e274df169b6d66c27cf
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-28 22:18:25 -0400

    Modificaciones al modelo tickets

--- Registro_Tickets_2024/Models/Tickets.cs
+++ Registro_Tickets_2024/Models/Tickets.cs
@@ -1,36 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Registro_Tickets_2024.Models
 {
     public class Tickets
     {
         [Key]
+        public int TicketId { get; set; }
+        [Required(ErrorMessage = "El campo fecha es obligatorio")]
+        public DateTime Fecha { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Selecciona una Prioridad")]
+        [ForeignKey("Prioridades")]
+        public int PrioridadId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Selecciona un Cliente")]
+        [ForeignKey("Clientes")]
         public int ClienteId { get; set; }
 
-        [Required(ErrorMessage = "El Nombre es un campo obligatorio.")]
-        [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
-        public string? Nombre { get; set; }
+        public int SistemaId { get; set; }
 
-        [Required(ErrorMessage = "El Teléfono es un campo obligatorio.")]
-        [StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "La longitud debe ser de 10 dígitos")]
-        [RegularExpression("^[0-9]+$", ErrorMessage = "El Numero de teléfono solo puede contener dígitos.")]
-        public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo Solicitado por es obligatorio")]
+        [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Solo debe contener letras.")]
+        public string? Solicitadopor { get; set; }
 
-        [Required(ErrorMessage = "El Celular es un campo obligatorio.")]
-        [StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "La longitud debe ser de 10 dígitos")]
-        [RegularExpression("^[0-9]+$", ErrorMessage = "El Celular solo puede contener digitos.")]
-        public string? Celular { get; set; }
+        [Required(ErrorMessage = "El campo Asunto es obligatorio")]
+        public string? Asunto { get; set; }
 
-        [Required(ErrorMessage = "El RNC es un campo obligatorio.")]
-        [StringLength(maximumLength: 9, MinimumLength = 9, ErrorMessage = "La longitud debe ser de 9 dígitos")]
-        [RegularExpression("^[0-9]+$", ErrorMessage = "El RNC solo puede contener digitos.")]
-        public string? RNC { get; set; }
-
-        [Required(ErrorMessage = "El correo electrónico es un campo obligatorio.")]
-        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
-        public string? Email { get; set; }
-
-        [Required(ErrorMessage = "La dirección es un campo obligatorio.")]
-        public string? Direccion { get; set; }
+        [Required(ErrorMessage = "El campo Descripción es obligatorio")]
+        public string? Descripcion { get; set; }
     }
 }
