861d75999fff6609f799db19344f3fd7561ce404
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-28 22:21:45 -0400

    Creamos nuestro contexto

---
+++ Registro_Tickets_2024/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Tickets_2024.Models;
+
+namespace Registro_Tickets_2024.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Clientes> clientes { get; set; }
+        public DbSet<Tickets> tickets { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+    }
+}
