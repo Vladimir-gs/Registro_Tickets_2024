0411cf110aa527a7ec46c0a3d7e863815f0d9a53
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-28 22:35:44 -0400

    Solucionamos una pequeño error en el contexto

--- Registro_Tickets_2024/DAL/Contexto.cs
+++ Registro_Tickets_2024/DAL/Contexto.cs
@@ -6,8 +6,8 @@ namespace Registro_Tickets_2024.DAL
     public class Contexto: DbContext
     {
         public DbSet<Prioridades> Prioridades { get; set; }
-        public DbSet<Clientes> clientes { get; set; }
-        public DbSet<Tickets> tickets { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
 
         public Contexto(DbContextOptions<Contexto> options): base(options) { }
     }
